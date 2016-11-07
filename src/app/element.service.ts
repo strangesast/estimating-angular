afaf62da504bb9bab0e04f8cd02790e9a665fd0d
Sam Zagrobelny <Samuel.Zagrobelny@dayautomation.com>
2016-11-07 15:58:29 -0500

    in progress

--- src/app/element.service.ts
+++ src/app/element.service.ts
@@ -15,13 +15,39 @@ import * as gitFormats      from 'js-git/mixins/formats';
 // useful
 //indexedDB.webkitGetDatabaseNames().onsuccess = (res) => {console.log([].slice.call(res.target.result).forEach((e)=>{indexedDB.deleteDatabase(e)}))}
 
+function streamify(stream): Promise<any[]> {
+  return new Promise((resolve, reject) => {
+    let fn = (arr) => {
+      stream.read((err, result) => {
+        if(err) return reject(err); // error out
+        if(result == null) return resolve(arr); // done
+        fn(arr.concat(result));
+      });
+    };
+    fn([]);
+  });
+};
+
+function promisify(...args: any[]): Promise<any[]> {
+  let fn = args[0];
+  return new Promise((resolve, reject) => {
+    let cb = function(err) {
+      if(err) return reject(err);
+      resolve([].slice.call(arguments, 1))
+    };
+    fn.apply(null, args.slice(1).concat(cb));
+  });
+}
+
+
 function random():string {
   return (Math.random().toString(36)+'00000000000000000').slice(2, 10+2);
 }
 
 // account of the user editing / saving job / components
 class User {
-  id: string;
+  //id?: string;
+  _id?: string;
   name: string;
   username: string;
   email: string;
@@ -81,103 +107,398 @@ class Job extends Element {
   shortname: string;
   basedOn?: BasedOn|null;    // potentially ambigious, null vs undefined
   folders: FolderDef;
-}
-
-function createRepo(prefix: string): any {
-  let repo = {};
-  gitIndexedDb(repo, prefix);
-  gitCreateTree(repo);
-  gitFormats(repo);
-  gitPackOps(repo);
-  gitReadCombiner(repo);
-  gitWalkers(repo);
-  return repo;
-}
-
-function readRefs(repo): string[] {
-  return ['a', 'b'];
+  commit?: string;
 }
 
 @Injectable()
 export class ElementService {
   activeUser: string | null = null;
-  indexedDbName: string = 'estimating';
-  indexedDbVersion: number = 1;
-  indexedDbRefPrefix: string = 'testing';
+
+  objectStoreName : string = 'estimating';
+  objectStoreVersion : number = 1;
+
+  gitStoreName: string = 'estimating-git';
+  gitStoreVersion: number = 1;
+  gitStoreRefPrefix: string = 'testing';
+
   repo: any;
+  gitdb: any;
   db: any;
 
   testRefs: string[] = ['test1', 'test2', 'test3'];
 
-  public users: BehaviorSubject<string[]> = new BehaviorSubject([]);
-  public projects: BehaviorSubject<string[]> = new BehaviorSubject([]);
+  public users: BehaviorSubject<User[]> = new BehaviorSubject([]);
+  public jobs: BehaviorSubject<Job[]> = new BehaviorSubject([]);
 
-  init(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      gitIndexedDb.init(this.indexedDbName, this.indexedDbVersion, (err, db) => {
-        if(err) return reject(err);
-        this.repo = createRepo(this.indexedDbRefPrefix);
-        this.db = db;
+  init(): Promise<any> {
+    return Promise.all([this.initObjectStore(), this.createGitRepo()]).then(() => {
+      let user: User = {
+        name: 'Sam Zagrobelny',
+        username: 'sazagrobelny',
+        email: 'sazagrobelny@dayautomation.com'
+      };
 
-        this.readRefs().then((refs) => {
-          let initUsers = [];
-          let initProjects = [];
-          for(let i=0; i<refs.length; i++) {
-            let a = refs[i].split('/');
-            if(a[0] == this.indexedDbRefPrefix) {
-              let username = a[1];
-              let projectShortId = a[2];
-              if(initUsers.indexOf(username) == -1) initUsers.push(username);
-              initProjects.push(initProjects);
+      let job: Job = {
+        id: random(),
+        name: 'Test Job 123',
+        description: '',
+        owner: user,
+        shortname: 'test_job_123',
+        folders: {
+          types: ['phase', 'building']
+        }
+      };
+
+      Promise.resolve().then(()=> {
+        return this.getUsers().then((users) => {
+          let usernames = users.map((u)=>u.username);
+          if(usernames.indexOf(user.username) == -1) {
+            return this.saveUser(user);
+          }
+        });
+      }).then(() => {
+        return this.getJobs().then((jobs)=>{
+
+          if(jobs.map((j)=>j.shortname).indexOf(job.shortname) == -1) {
+            return this.saveNewJob(job);
+          }
+        }).then(()=>{
+          this.getJobs().then((jobs)=>{
+            let job = jobs[0];
+
+            job.description = 'new description';
+
+            return this.saveJob(job, 'updated description');
+
+          }).then((hash)=>{
+            console.log(hash);
+
+          });
+        });
+      });
+
+    });
+  }
+
+  debugCommit(hash: string) : void {
+    this.loadAs('commit', hash).then((commit) => {
+      let promises = [];
+      let recurse = (tree) => {
+        for(var prop in tree) {
+          let el = tree[prop];
+          let prom = Promise.resolve();
+          if(el.mode == gitModes.blob) {
+            prom = this.loadAs('text', el.hash).then((res)=> {
+              el.body = res;
+            });
+          } else if (el.mode == gitModes.tree) {
+            prom = this.loadAs('tree', el.hash).then((res)=> {
+              el.body = res;
+              return recurse(el.body);
+            });
+          }
+          promises.push(prom);
+        }
+      }
+
+      let recurse2 = (tree) => {
+        if(typeof tree == 'string') {
+          return JSON.parse(tree);
+        } else if (typeof tree == 'object') {
+          let ob = {};
+          for(let prop in tree) { 
+            console.log(tree[prop].body)
+            if(tree[prop].body) {
+              ob[prop] = recurse2(tree[prop].body);
             }
           }
-          this.users.next(initUsers);
-          this.projects.next(initProjects);
+          return ob;
+        }
+      };
 
-          return resolve({users: initUsers, projects: initProjects});
+      this.loadAs('tree', commit.tree).then((tree)=>{
+        recurse(tree);
+        Promise.all(promises).then(()=>{
+          console.log(recurse2(tree));
         });
       });
     });
   }
 
-  readRefs(): Promise<string[]> {
+  getJobs(): Promise<Job[]> {
+    return this.readRefs(this.gitdb).then((refs) => {
+      // read all refs -> many may point to same job
+      return Promise.all(refs.map((ref)=>this.readRef(ref)));
+
+    }).then((jobs) => {
+      return Promise.all(jobs.map((commitHash)=>{
+        return this.loadAs('commit', commitHash).then((commit) => {
+          return this.loadAs('tree', commit.tree);
+        }).then((tree)=>{
+          return this.loadAs('text', tree['job.json'].hash).then((text)=>{
+            let job : Job = JSON.parse(text);
+            job.commit = commitHash;
+            return this.saveToStore(this.db, 'jobs', job).then((key)=>{
+              return job;
+            });
+          });
+        });
+      }));
+    });
+  };
+
+  getUsers(): Promise<User[]> {
+    let db = this.db;
+    if(db == null) throw new Error('db undefined, run init');
+    return this.getAll(db, 'users');
+  }
+
+  getAll(db: any, storeName: string): Promise<any[]> {
     return new Promise((resolve, reject) => {
-      if(this.db == null) throw new Error('db undefined, run init');
-      let storeNames = [].slice.call(this.db.objectStoreNames);
-      if(storeNames.indexOf('refs') == -1) {
+      let trans = db.transaction([storeName]);
+      let store = trans.objectStore(storeName);
+      let req = store.getAll();
+      req.onsuccess = (e) => {
+        resolve(e.target.result);
+      };
+      req.onerror = (e) => {
+        reject(e.target.error);
+      };
+    });
+  }
+
+  saveToStore(db: any, storeName: string, obj: any): Promise<string> {
+    return new Promise((resolve, reject) => {
+      let trans = db.transaction(storeName, 'readwrite');
+      let store = trans.objectStore(storeName);
+      let req = store.put(obj);
+      req.onsuccess = (e) => {
+        resolve(e.target.result)
+      };
+      req.onerror = (e) => {
+        reject(e.target.error);
+      }
+    });
+  }
+
+  // returns keypath of created user
+  saveUser(user: User): Promise<string> {
+    return this.saveToStore(this.db, 'users', user);
+  }
+
+  readUsersJobs(): Promise<any> {
+    return this.readRefs(this.gitdb).then((refs) => {
+      let initUsers = [];
+      let initProjects = [];
+      for(let i=0; i<refs.length; i++) {
+        let a = refs[i].split('/');
+        if(a[0] == this.gitStoreRefPrefix) {
+          let username = a[1];
+          let projectShortId = a[2];
+          if(initUsers.indexOf(username) == -1) initUsers.push(username);
+          initProjects.push(initProjects);
+        }
+      }
+      this.users.next(initUsers);
+      this.jobs.next(initProjects);
+
+      return {users: initUsers, projects: initProjects};
+
+    });
+  }
+
+  initObjectStore(): Promise<any> {
+    let name = this.objectStoreName;
+    let version = this.objectStoreVersion;
+    return new Promise((resolve, reject) => {
+      let request = indexedDB.open(name, version);
+
+      request.onupgradeneeded = (e:any) => {
+        let db = e.target.result;
+
+        let stores = ['users', 'components', 'folders'];
+        for(let i=0,store; store=stores[i],i<stores.length; i++) {
+          if(db.objectStoreNames.contains(store)) {
+            db.deleteObjectStore(store);
+          }
+        }
+        Promise.all([
+          new Promise((resolve) => {
+            // users
+            let userStore = db.createObjectStore('users', {keyPath: 'username'});
+            userStore.createIndex('name',      'name',     {unique: false});
+            //userStore.createIndex('username',  'username', {unique: true});
+            userStore.createIndex('email',     'email',    {unique: true});
+            userStore.transaction.oncomplete = (e) => {
+              resolve();
+            };
+          }),
+          new Promise((resolve) => {
+            // components
+            let componentStore = db.createObjectStore('components', {keyPath: 'id'});
+            componentStore.createIndex('children', 'children', { unique: false, multiEntry: true });
+            componentStore.transaction.oncomplete = (e) => {
+              resolve();
+            };
+          }),
+          new Promise((resolve) => {
+            let store = db.createObjectStore('jobs', {keyPath: 'id'});
+            store.createIndex('shortname', 'shortname', { unique: true });
+            store.transaction.oncomplete = (e) => {
+              resolve();
+            }
+          }),
+          new Promise((resolve) => {
+            let folderStore = db.createObjectStore('folders', {keyPath: 'id'});
+            folderStore.transaction.oncomplete = (e) => {
+              resolve();
+            }
+          })
+        ]).then(()=>{
+
+          this.db = db;
+          resolve(db);
+        }); // may not resolve
+      };
+
+      request.onsuccess = (e:any) => {
+        let db = e.target.result;
+        this.db = db;
+        resolve(db);
+      };
+    });
+  }
+
+  createGitRepo(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      gitIndexedDb.init(this.gitStoreName, this.gitStoreVersion, (err, db) => {
+        if(err) return reject(err);
+        let repo = {};
+        gitIndexedDb(repo, this.gitStoreRefPrefix);
+        gitCreateTree(repo);
+        gitFormats(repo);
+        gitPackOps(repo);
+        gitReadCombiner(repo);
+        gitWalkers(repo);
+
+        this.repo = repo
+        this.gitdb = db;
+        resolve(this.repo);
+      });
+    });
+  }
+
+  readRefs(db: any): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+      if(db == null) throw new Error('db undefined, run init');
+      if(!db.objectStoreNames.contains('refs')) {
         return reject(new Error('refs uninitialized, run init'));
       }
-      let trans = this.db.transaction(['refs'], 'readwrite');
-      trans.oncomplete = (e) => {
-        console.log('transaction complete', e);
-      };
-      trans.onerror = (e) => {
-        console.log('transaction error', e);
-        return reject(e);
-      };
+      let trans = db.transaction(['refs']);
       let store = trans.objectStore('refs');
       let req = store.getAllKeys();
       req.onsuccess = (e) => {
-        return resolve(e.target.result);
+        resolve(e.target.result);
       };
+      req.onerror = (e) => {
+        reject(e.target.error);
+      }
     }).then((arr: string[]) => {
       // filter out different prefixes
       return arr.filter((str) => {
-        return str.startsWith(this.indexedDbRefPrefix) && str.split('/')[0] == this.indexedDbRefPrefix;
+        return str.startsWith(this.gitStoreRefPrefix) && str.split('/')[0] == this.gitStoreRefPrefix;
       }).map((str)=>{
-        return str.substring(this.indexedDbRefPrefix.length + 1);
+        return str.substring(this.gitStoreRefPrefix.length + 1);
       });
     });
   }
 
   readRef(ref: string):Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.repo.readRef(ref, (err, hash) => {
-        if(err) return reject(err);
-        resolve(hash);
-      });
+    return promisify(this.repo.readRef.bind(this.repo), ref).then((res)=>{
+      return res[0];
     });
   }
+
+  getJobTree(job: Job): Promise<any> {
+    return Promise.resolve();
+  }
+
+  saveNewJob(job: Job): Promise<any> {
+    let jobText = JSON.stringify(job);
+    let tree = {
+      'job.json': {
+        mode: gitModes.file,
+        content: jobText
+      }
+    };
+    let folderTypes = job.folders.types;
+    for(let i=0,folderType; folderType=folderTypes[i],i<folderTypes.length; i++) {
+      let folder: Folder = {
+        id: random(),
+        job: job.id,
+        type: folderType,
+        name: 'root',
+        children: []
+      };
+      let folderText = JSON.stringify(folder);
+      let folderPath = [folderType, folder.id + '.json'].join('/');
+      tree[folderPath] = {
+        mode: gitModes.file,
+        content: folderText
+      };
+    }
+    return promisify(this.repo.createTree.bind(this.repo), tree).then((both)=>{
+      let hash = both[0], tree = both[1];
+
+      return this.saveToHash('commit', {
+        author: {
+          name: job.owner.username,
+          email: job.owner.email
+        },
+        tree: hash,
+        message: 'first'
+      });
+    }).then((commit)=>{
+      let ref = [job.owner.username, job.shortname].join('/');
+
+      return this.updateRef(ref, commit);
+    });
+  };
+
+  saveJob(job: Job, message: string): Promise<any> {
+    if(job.commit == null || job.commit == '') throw new Error('job must have first commit');
+    return this.loadAs('commit', job.commit).then((commit) => {
+      let jobText = JSON.stringify(job);
+      let changes:any = [{
+        path: 'job.json',
+        mode: gitModes.file,
+        content: jobText
+      }];
+      changes.base = commit.tree;
+
+      return promisify(this.repo.createTree.bind(this.repo), changes).then((res)=>{
+        return res[0];
+      });
+
+    }).then((treeHash)=> {
+      console.log('new tree', treeHash);
+      return this.saveToHash('commit', {
+        author: {
+          name: job.owner.username,
+          email: job.owner.email
+        },
+        tree: treeHash,
+        message: message,
+        parents: [job.commit]
+      }).then((commitHash)=> {
+        let ref = [job.owner.username, job.shortname].join('/');
+
+        return this.updateRef(ref, commitHash).then(()=>{
+          return commitHash;
+        });
+      });
+    });
+  };
 
   saveEmptyJob(shortname: string, name: string, description: string, owner: User): Promise<void> {
     if(/\s/g.test(shortname)) throw new Error('invalid job shotname - no spaces');
@@ -199,8 +520,7 @@ export class ElementService {
     let jobBlob:string = JSON.stringify(job);
     let ref = [job.owner.username, job.shortname].join('/');
 
-    return this.readRefs().then((refs: string[]) => {
-      console.log('refs', refs);
+    return this.readRefs(this.gitdb).then((refs: string[]) => {
       if(refs.indexOf(ref) != -1) throw new Error('username / job id combination already exists');
 
       // create root phase / building
@@ -248,7 +568,6 @@ export class ElementService {
         tree[pair[0]] = { mode: gitModes.tree, hash: pair[1] };
       });
 
-      console.log(tree);
 
       return this.saveToHash('tree', tree);
 
@@ -268,24 +587,22 @@ export class ElementService {
 
     }).catch((err) => {
       console.error('error!', err);
+
+
     }).then(() => {
       this.readRef(ref).then((commitHash) => {
-        console.log('commit', commitHash);
+
         return this.logWalk(commitHash);
-      }).then((stream) => {
-        new Promise((resolve, reject) => {
-          function recurse(s, arr) {
-            arr = arr || [];
-            s.read((err, result) => {
-              if(err) return reject(err);
-              if(result == null) return resolve(arr);
-              recurse(s, arr.concat(result));
-            });
-          };
-        }).then((result) => {
-          console.log('result', result);
-        });
-        return;
+
+      }).then(streamify).then((arr) => {
+        // arr = commits
+
+        return promisify(this.repo.treeWalk.bind(this.repo), arr[0].tree)
+      }).then((result) => {
+        // args applied to cb
+        let stream = result[0]
+        return streamify(stream);
+      }).then((result) => {
       });
     });
   }
@@ -299,48 +616,36 @@ export class ElementService {
     });
   }
 
-  saveToHash(kind: 'blob'|'tree'|'commit', body: any):Promise<string> {
+  treeWalk(hash: string): Promise<any> {
     return new Promise((resolve, reject) => {
-      this.repo.saveAs(kind, body, (err, hash, body) => {
+      this.repo.treeWalk(hash, (err, stream) => {
         if(err) return reject(err);
-        resolve(hash);
+        resolve(stream);
       });
+    });
+  }
+
+  loadAs(kind: 'blob'|'tree'|'commit'|'text', hash: string):Promise<any>{
+    return promisify(this.repo.loadAs.bind(this.repo), kind, hash).then((res) => {
+      return res[0]; //=body, res[1]=hash
+    });
+  }
+
+  saveToHash(kind: 'blob'|'tree'|'commit', body: any):Promise<string> {
+    return promisify(this.repo.saveAs.bind(this.repo), kind, body).then((res) => {
+      return res[0];
     });
   }
 
   updateRef(ref: string, hash: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.repo.updateRef(ref, hash, (err) => {
-        if(err) return reject(err);
-        resolve();
-      });
+    // repo.updateRef doesn't return anything
+    return promisify(this.repo.updateRef.bind(this.repo), ref, hash).then((res)=>{
+      return null;
     });
   }
 
   constructor() {
-    this.init().then(() => {
-      let id = random();
-      let id2 = random();
-      console.log(id, id2);
-
-      let user: User = {
-        id: id2,
-        name: 'Sam Zagrobelny',
-        username: 'sazagrobelny',
-        email: 'sazagrobelny@dayautomation.com'
-      };
-
-      //                short id        name            desc                     owner
-      this.saveEmptyJob('test_job_123', 'Test job 123', 'this is a description', user).then(() => {
-
-        console.log('done saving..');
-
-      }).catch((err)=>{
-
-        console.log('failed to save new job', err);
-
-      });
-    });
+    this.init();
   }
 
 }
