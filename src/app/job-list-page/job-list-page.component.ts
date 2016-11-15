bad5ebea1ae969c023e86d1467132004f070d016
Sam Zagrobelny <Samuel.Zagrobelny@dayautomation.com>
2016-11-15 09:17:39 -0500

    started styling

--- src/app/job-list-page/job-list-page.component.ts
+++ src/app/job-list-page/job-list-page.component.ts
@@ -4,59 +4,6 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { ElementService } from '../element.service';
 
 import { User, Job } from '../classes';
-//class User {
-//  id: number;
-//  name: string;
-//  username: string;
-//  email: string;
-//}
-//
-//class Job {
-//  id: number;
-//  name: string;
-//  description: string;
-//  start_date?: Date;
-//  end_date?: Date;
-//  owner: number;
-//  local: boolean;
-//  remote: boolean;
-//}
-
-//const USERS: User[] = [
-//  {
-//    id: 0,
-//    name: 'Sam Zagrobelny',
-//    username: 'sazagrobelny',
-//    email: 'Samuel.Zagrobelny@dayautomation.com'
-//  }
-//];
-
-//var date1 = new Date();
-//var date2 = new Date();
-//date2.setDate(date1.getDate() - 1);
-//
-//const JOBS: Job[] = [
-//  {
-//    id: 1,
-//    name: 'Job 123',
-//    description: 'This job is really great.',
-//    start_date: date1,
-//    end_date: date2,
-//    owner: USERS[0].id,
-//    local: true,
-//    remote: false
-//  },
-//  {
-//    id: 2,
-//    name: 'Job 234',
-//    description: 'This job is really great.',
-//    start_date: date1,
-//    end_date: date2,
-//    owner: USERS[0].id,
-//    local: true,
-//    remote: true
-//  }
-//];
 
 @Component({
   selector: 'app-job-list-page',
@@ -64,8 +11,8 @@ import { User, Job } from '../classes';
   styleUrls: ['./job-list-page.component.less']
 })
 export class JobListPageComponent implements OnInit {
-  jobs: BehaviorSubject<Job[]> = new BehaviorSubject([]);
-  users: BehaviorSubject<User[]> = new BehaviorSubject([]);
+  jobs: Job[] = [];
+  users: User[] = [];
 
   constructor(private elementService: ElementService) { }
 
@@ -73,12 +20,10 @@ export class JobListPageComponent implements OnInit {
     this.elementService.init().then(()=>{
       return Promise.all([
         this.elementService.getJobs().then((jobs: Job[])=> {
-          console.log('jobs:', jobs);
-          this.jobs.next(jobs);
+          this.jobs = jobs;
         }),
         this.elementService.getUsers().then((users: User[])=>{
-          console.log('users:', users);
-          this.users.next(users);
+          this.users = users;
         })
       ]);
     });
