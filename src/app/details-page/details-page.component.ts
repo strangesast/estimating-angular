12f69f0532712b829086827e6c4fa57fbd234346
Sam Zagrobelny <Samuel.Zagrobelny@dayautomation.com>
2016-12-06 09:13:12 -0500

    finished example details form

--- src/app/details-page/details-page.component.ts
+++ src/app/details-page/details-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { JobService } from '../job.service';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { Job } from '../classes';
 
 
@@ -12,13 +14,37 @@ import { Job } from '../classes';
 }) export class DetailsPageComponent implements OnInit {
   private sub: Subscription;
   private job: Job;
+  private jobForm: FormGroup;
+
+  private availableUsers = [
+    {name: 'Sam Zagrobelny', username: 'sazagrobelny', id: '1231231231231'}
+  ];
 
   constructor(
-    private jobService: JobService
+    private jobService: JobService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
     this.sub = this.jobService.job.subscribe(job => {
+      this.jobForm = this.formBuilder.group({
+        name: [job.name, [
+          Validators.minLength(5),
+          Validators.required]
+        ],
+        description: job.description,
+        owner: this.formBuilder.group({
+          name: [job.owner.name, Validators.required],
+          username: [job.owner.username, [Validators.required, Validators.pattern('^[A-Za-z0-9]+$')]],
+          email: [job.owner.email, Validators.required],
+        }),
+        shortname: [job.shortname, [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.pattern('^[A-Za-z0-9-_]+$')]
+        ]
+      });
+
       this.job = job;
       this.jobService.findChanges().then(hist => {
         console.log('history', hist);
@@ -29,5 +55,22 @@ import { Job } from '../classes';
     this.sub.unsubscribe();
   }
 
+  reset() {
+    this.jobForm.reset();
+    this.jobForm.setValue({
+      name: this.job.name,
+      description: this.job.description,
+      owner: {
+        name: this.job.owner.name,
+        username: this.job.owner.username,
+        email: this.job.owner.email
+      },
+      shortname: this.job.shortname
+    })
+  }
 
+  onSubmit(job: Job) {
+    console.log(this.jobForm);
+    console.log(job);
+  }
 }
