108dd4c2104191fc0daac7b3e416fb407a408e25
Sam Zagrobelny <Samuel.Zagrobelny@dayautomation.com>
2016-12-16 14:53:38 -0500

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,3 @@
-import 'rxjs/Rx';
-import './rxjs-extensions';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
